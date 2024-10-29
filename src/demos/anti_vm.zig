5acc934e73f03635f1197a5fc86a0687eb32f8b7
soheil <soheil.rasekh.01@gmail.com>
2024-10-29 15:38:22 +0330

    feat: Anti Virtualization via machine resolution

--- src/demos/anti_vm.zig
+++ src/demos/anti_vm.zig
@@ -3,4 +3,5 @@ const sec = @import("zig-sec");
 
 pub fn main() !void {
     std.debug.print("[!] isVenvByHardwareCheck: {}\n", .{try sec.anti_vm.isVenvByHardwareCheck()});
+    std.debug.print("[!] checkMachineResolution: {}\n", .{try sec.anti_vm.checkMachineResolution()});
 }
--- src/utils/anti_vm.zig
+++ src/utils/anti_vm.zig
@@ -7,12 +7,20 @@ const KEY_READ = win.KEY_READ;
 const SYSTEM_INFO = win.SYSTEM_INFO;
 const MEMORYSTATUSEX = win.MEMORYSTATUSEX;
 const HKEY = win.HKEY;
+const HMONITOR = win.HMONITOR;
+const HDC = win.HDC;
+const RECT = win.RECT;
+const LPARAM = win.LPARAM;
+const BOOL = win.BOOL;
+const MONITORINFO = win.MONITORINFO;
 
 const GetSystemInfo = win.GetSystemInfo;
 const GlobalMemoryStatusEx = win.GlobalMemoryStatusEx;
 const RegOpenKeyExA = win.RegOpenKeyExA;
 const RegQueryInfoKeyA = win.RegQueryInfoKeyA;
 const GetLastError = win.GetLastError;
+const EnumDisplayMonitors = win.EnumDisplayMonitors;
+const GetMonitorInfoW = win.GetMonitorInfoW;
 
 pub fn isVenvByHardwareCheck() !bool {
     var system_info: SYSTEM_INFO = undefined;
@@ -83,4 +91,49 @@ pub fn isVenvByHardwareCheck() !bool {
     }
 
     return cpu_check or ram_check or usb_check;
+}
+
+fn resolutionCallback(
+    h_monitor: ?HMONITOR,
+    _: ?HDC,
+    _: ?*RECT,
+    data: usize,
+) callconv(std.os.windows.WINAPI) BOOL {
+    var monitor_info = std.mem.zeroes(MONITORINFO);
+    monitor_info.cbSize = @sizeOf(MONITORINFO);
+
+    if (GetMonitorInfoW(h_monitor, &monitor_info) == 0) {
+        std.debug.print("[!] GetMonitorInfoW Failed With Error: {s}\n", .{@tagName(GetLastError())});
+        return 0;
+    }
+
+    var x = monitor_info.rcMonitor.right - monitor_info.rcMonitor.left;
+    if (x < 0) x = -x;
+
+    var y = monitor_info.rcMonitor.top - monitor_info.rcMonitor.bottom;
+    if (y < 0) y = -y;
+
+    if ((x != 1920 and x != 2560 and x != 1440) or (y != 1080 and y != 1200 and y != 1600 and y != 900)) {
+        std.debug.print("[!] Resolution {d}x{d} Found.\n", .{ x, y });
+        const sandbox_check: *bool = @ptrFromInt(data);
+        sandbox_check.* = true;
+    }
+
+    return 1;
+}
+
+pub fn checkMachineResolution() !bool {
+    var sandbox_check = false;
+
+    if (EnumDisplayMonitors(
+        null,
+        null,
+        @ptrCast(&resolutionCallback),
+        @bitCast(@intFromPtr(&sandbox_check)),
+    ) == 0) {
+        std.debug.print("[!] EnumDisplayMonitors Failed With Error: {s}\n", .{@tagName(GetLastError())});
+        return error.EnumDisplayMonitorsFailed;
+    }
+
+    return sandbox_check;
 }
